8f2d9396a9aef370a145293cee8a4a5ca3b2e4c9
Azizah <aziza.helmy35@gmail.com>
2024-04-16 17:33:51 +0200

    add dropdown menu in HomeScreen.kt

--- app/src/main/java/com/example/dailyforecast/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/dailyforecast/ui/screen/HomeScreen.kt
@@ -6,16 +6,25 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -37,16 +46,66 @@ fun HomeScreen(viewModel: HomeViewModel = koinInject()) {
 
 @Composable
 private fun HomeContent(state: HomeUiState) {
-    LazyColumn(
-        modifier = Modifier.background(Color(0xFFF2F2F2)),
-        contentPadding = PaddingValues(
-            horizontal = 16.dp,
-            vertical = 4.dp
-        )
-    ) {
-        itemsIndexed(items = state.weatherItems) { _, user ->
-            WeatherItem(user)
+    Column {
+        DropDown(state)
+        LazyColumn(
+            modifier = Modifier.background(Color(0xFFF2F2F2)),
+            contentPadding = PaddingValues(
+                horizontal = 16.dp,
+                vertical = 4.dp
+            )
+        ) {
+            itemsIndexed(items = state.weatherItems) { _, user ->
+                WeatherItem(user)
+            }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DropDown(state: HomeUiState) {
+
+    var selectedText by remember {
+        mutableStateOf(state.cities[0].cityNameAr)
+    }
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 24.dp, bottom = 8.dp)
+    ) {
+        ExposedDropdownMenuBox(
+            expanded = isExpanded,
+            onExpandedChange = { isExpanded = !isExpanded }) {
+
+            TextField(
+                modifier = Modifier.menuAnchor(),
+                value = selectedText,
+                onValueChange = {},
+                readOnly = true,
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(
+                        expanded = isExpanded
+                    )
+                })
+
+            ExposedDropdownMenu(expanded = isExpanded, onDismissRequest = { isExpanded = false }) {
+                state.cities.forEachIndexed { index, text ->
+                    DropdownMenuItem(
+                        text = { Text(text = text.cityNameAr) },
+                        onClick = {
+                            selectedText = state.cities[index].cityNameAr
+                            isExpanded = false
+                        }, contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                    )
+                }
+            }
+        }
+        Text(text = "Current selected $selectedText", modifier = Modifier.padding(vertical = 16.dp))
     }
 }
 
