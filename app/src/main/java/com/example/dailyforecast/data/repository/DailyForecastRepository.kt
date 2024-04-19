6dff23f334a6ae23e1af740f01f55745080aeaaf
Azizah <aziza.helmy35@gmail.com>
2024-04-19 02:15:18 +0200

    remove unnecessary code

--- app/src/main/java/com/example/dailyforecast/data/repository/DailyForecastRepository.kt
+++ app/src/main/java/com/example/dailyforecast/data/repository/DailyForecastRepository.kt
@@ -7,7 +7,6 @@ import com.example.dailyforecast.data.source.local.model.CityList
  * Created by Aziza Helmy on 4/15/2024.
  */
 interface DailyForecastRepository {
-    suspend fun getWeather()
     suspend fun getWeatherFromRemote(lat: Double, long: Double): List<WeatherItem>
     suspend fun insertAllDailyForecastToDb(dailyForecast: List<WeatherItem>)
     suspend fun getAllDailyForecastFromDb():List<WeatherItem>
